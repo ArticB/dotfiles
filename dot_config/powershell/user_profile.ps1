d20837dd79b986555d71b704258f5f85c28f4a90
Deepraj Pradhan <deeprajpradhan@live.com>
2022-07-12 13:02:52 +0530

    Initial commit

---
+++ dot_config/powershell/user_profile.ps1
@@ -0,0 +1,30 @@
+# Prompt
+Import-Module posh-git
+Import-Module Terminal-Icons
+
+#Load  prompt config
+oh-my-posh init pwsh --config "$env:USERPROFILE\.config\powershell\takuya.omp.json" | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias 
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
